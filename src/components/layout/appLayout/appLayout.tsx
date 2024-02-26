363605a6ffaa1f69901df35cc47cac31ebc4a3db
Thanapheem <63512677+Thanapheem@users.noreply.github.com>
2024-02-26 16:26:37 +0700

    feat: 🎸 initial

---
+++ src/components/layout/appLayout/appLayout.tsx
@@ -0,0 +1,222 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import {
+  MoveToInbox,
+  InboxOutlined,
+  SearchOutlined,
+  ExpandLess,
+  ExpandMore,
+  StarBorder,
+  PictureAsPdfOutlined,
+} from "@mui/icons-material";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+
+import {
+  Collapse,
+  Drawer,
+  List,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
+
+const pages = ["Products", "Pricing", "Blog"];
+const settings = ["Profile", "Account", "Dashboard", "Logout"];
+
+function AppLayout({ children }: { children: React.ReactElement }) {
+  const [collapse, setCollapes] = React.useState(false);
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
+    null
+  );
+  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
+    null
+  );
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  return (
+    <Box sx={{ display: "flex" }}>
+      <AppBar
+        color="secondary"
+        position="fixed"
+        sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
+      >
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            <MoveToInbox sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
+            <Typography
+              variant="h6"
+              noWrap
+              component="a"
+              href="#app-bar-with-responsive-menu"
+              sx={{
+                mr: 2,
+                display: { xs: "none", md: "flex" },
+                fontFamily: "monospace",
+                fontWeight: 700,
+                letterSpacing: ".3rem",
+                color: "inherit",
+                textDecoration: "none",
+              }}
+            >
+              LOGO
+            </Typography>
+
+            <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenNavMenu}
+                color="inherit"
+              >
+                <MoveToInbox />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                  vertical: "bottom",
+                  horizontal: "left",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "left",
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{
+                  display: { xs: "block", md: "none" },
+                }}
+              >
+                {pages.map((page) => (
+                  <MenuItem key={page} onClick={handleCloseNavMenu}>
+                    <Typography textAlign="center">{page}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+            <MoveToInbox sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
+            <Typography
+              variant="h5"
+              noWrap
+              component="a"
+              href="#app-bar-with-responsive-menu"
+              sx={{
+                mr: 2,
+                display: { xs: "flex", md: "none" },
+                flexGrow: 1,
+                fontFamily: "monospace",
+                fontWeight: 700,
+                letterSpacing: ".3rem",
+                color: "inherit",
+                textDecoration: "none",
+              }}
+            >
+              LOGO
+            </Typography>
+            <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+              
+            </Box>
+
+            <Box sx={{ flexGrow: 0 }}>
+              <Tooltip title="Open settings">
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                  <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                </IconButton>
+              </Tooltip>
+              <Menu
+                sx={{ mt: "45px" }}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+              >
+                {settings.map((setting) => (
+                  <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                    <Typography textAlign="center">{setting}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+      <Drawer
+        sx={{
+          width: 200,
+          flexShrink: 0,
+          "& .MuiDrawer-paper": {
+            width: 200,
+            boxSizing: "border-box",
+          },
+        }}
+        variant="permanent"
+        anchor="left"
+      >
+        <Toolbar />
+
+        <List>
+          <ListItemButton onClick={() => setCollapes(!collapse)}>
+            <ListItemIcon sx={{ minWidth: "32px" }}>
+              <SearchOutlined />
+            </ListItemIcon>
+            <ListItemText primary="Inquiry Activities" />
+            {!collapse ? <ExpandLess /> : <ExpandMore />}
+          </ListItemButton>
+          <Collapse in={!collapse} timeout="auto" unmountOnExit>
+            <List component="div" disablePadding>
+              <ListItemButton sx={{ pl: 6 }}>
+                <ListItemText primary="Transaction Information" />
+              </ListItemButton>
+            </List>
+          </Collapse>
+          <ListItemButton onClick={() => setCollapes(!collapse)}>
+            <ListItemIcon sx={{ minWidth: "32px" }}>
+              <PictureAsPdfOutlined />
+            </ListItemIcon>
+            <ListItemText primary="Gen XML" />
+          </ListItemButton>
+        </List>
+      </Drawer>
+      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        <Toolbar />
+        <div className="shadow-lg p-4 h-full">{children}</div>
+      </Box>
+    </Box>
+  );
+}
+export default AppLayout;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,48 @@
 import "@/styles/globals.css";
+import { NextPage } from "next";
+import { AppCacheProvider } from "@mui/material-nextjs/v14-pagesRouter";
 import type { AppProps } from "next/app";
+import { ReactElement, ReactNode } from "react";
+import { enUS } from "date-fns/locale";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFnsV3";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { ThemeProvider, createTheme } from "@mui/material";
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+const theme = createTheme({
+  typography: {
+    fontSize: 12,
+  },
+  palette: {
+    primary: {
+      main: "#785FC9",
+      dark: "#4B3493",
+    },
+    secondary: {
+      main: "#211051",
+    },
+  },
+});
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? ((page) => page);
+
+  return (
+    <>
+      <AppCacheProvider {...pageProps}>
+        <ThemeProvider theme={theme}>
+          <LocalizationProvider
+            adapterLocale={enUS}
+            dateAdapter={AdapterDateFns}
+          >
+            {getLayout(<Component {...pageProps} />)}
+          </LocalizationProvider>
+        </ThemeProvider>
+      </AppCacheProvider>
+    </>
+  );
 }
